26a943c436672bcbc8aa39ca2859102e1b2aa04f
Gniot <orzesekyt@gmail.com>
2024-10-02 09:36:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function renderHomePage(lang) {
     const tresc = document.getElementById('tresc');
     tresc.innerHTML = `
         <h2>${translations[lang].homeTitle}</h2>
-        <p>${translations[lang].homeContent}</p>
+        <p>${translations[lang].Mapa}</p>
     `;
 }
 
