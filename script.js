19e0ff26dd761069f2c6900094b42cfa7ed6b97b
Okruch165 <oskarjablonski272@gmail.com>
2024-10-02 09:46:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function renderSettings(lang) {
 // Funkcja do renderowania treści w menu
 function renderMenu(lang) {
     const menuItems = document.querySelectorAll('#menu ul li');
-    menuItems[0].textContent = translations[lang].homeTitle; // Strona główna
+    menuItems[0].textContent = translations[lang].Mapa; // Strona główna
     menuItems[1].textContent = translations[lang].about; // O nas
     menuItems[2].textContent = translations[lang].gallery; // Galeria
     menuItems[3].textContent = translations[lang].settingsTitle; // Ustawienia
